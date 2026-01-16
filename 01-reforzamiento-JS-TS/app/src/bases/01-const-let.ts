e54167ddb81898f46d64877c5b6bb19e00662a9e
nicobertincf <nicolas.bertin@comunidadfeliz.cl>
2026-01-16 14:45:28 -0300

    13. Variables y constantes

---
+++ 01-reforzamiento-JS-TS/app/src/bases/01-const-let.ts
@@ -0,0 +1,11 @@
+const fistName: string = 'Nicolas';
+const lastName: string = 'Bertin';
+
+let diceNumber: number = 5;
+diceNumber = 6;
+
+console.log(fistName, lastName);
+
+const containsName: boolean = lastName.includes('Bertin');
+
+console.log({containsName, diceNumber});
